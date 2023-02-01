71d7bd6524beb421a0a923538d186f1c9e4a8867
sandeepkaurs <sandeep.singh@telus.com>
2023-01-31 17:47:45 -0800

    making additional changes to admin categories

--- app/controllers/admin/catergories_controller.rb
+++ app/controllers/admin/catergories_controller.rb
@@ -3,9 +3,17 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else 
+      render :new
+    end
   end
 
   def new
+    @categories = Category.new
   end
 
   def index
